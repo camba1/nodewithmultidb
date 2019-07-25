96df728d60f72ceaa9ec89894b153499fc93beee
Juan Peredo <juan@Juans-MacBook-Pro.local>
2019-07-25 18:44:52 -0500

    inital hello world node App running out of the dockerfile

---
+++ nodeApp/src/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const express = require('express');
+
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+//MySql connection configuration
+
+//Postgres connection configuration
+
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world!\n');
+});
+
+app.get('/mysql', (req, res) => {
+  res.send('Hello world2!\n');
+  }
+);
+
+app.get('/postgres', (req, res) => {
+  res.send('Hello world3!\n');
+  }
+);
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
