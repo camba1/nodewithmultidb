c73292be7b6f97473380d46d6bbae2ab855e5f3f
Juan Peredo <juan@Juans-MacBook-Pro.local>
2019-07-26 21:49:33 -0500

    moved mysql to connection polling

--- nodeApp/src/mySqlData.js
+++ nodeApp/src/mySqlData.js
@@ -1,24 +1,46 @@
 var mysql = require('mysql');
 
-var conn = mysql.createConnection({
-  host: 'nodemysql',
-  port: '3306',
-  user: 'root',
-  password: 'TestDB@home2',
-  database: 'testDB'
+// var conn = mysql.createConnection({
+//   host: 'nodemysql',
+//   port: '3306',
+//   user: 'root',
+//   password: 'TestDB@home2',
+//   database: 'testDB'
+// });
+//
+// const getMySqlData = (request, response) => {
+//   conn.connect((err) =>{
+//     if(err) throw err;
+//     console.log('Mysql Connected...');
+//   });
+//   conn.query('SELECT * FROM test ORDER BY id ASC', (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     response.status(200).json(results)
+//   })
+// }
+
+
+var pool = mysql.createPool({
+    connectionLimit : 10,
+    host: 'nodemysql',
+    port: '3306',
+    user: 'root',
+    password: 'TestDB@home2',
+    database: 'testDB'
 });
 
+
 const getMySqlData = (request, response) => {
-  conn.connect((err) =>{
-    if(err) throw err;
-    console.log('Mysql Connected...');
+  pool.getConnection(function (err, connection) {
+      connection.query("SELECT * FROM test ORDER BY id ASC", function (err, rows) {
+          connection.release();
+          if (err) throw err;
+          response.status(200).json(rows);
+      });
   });
-  conn.query('SELECT * FROM test ORDER BY id ASC', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results)
-  })
 }
+
 
 module.exports = { getMySqlData }
